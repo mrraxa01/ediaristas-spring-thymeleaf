1a19df68e6aad9c19f792c20bf831bfcfa0250da
Marcio Rodrigues <mrraxa01@gmail.com>
2024-05-22 22:03:21 -0300

    Implement action cadastrar()

--- src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
@@ -1,17 +1,24 @@
 package br.com.treinaweb.ediaristas.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.ediaristas.core.enums.Icone;
 import br.com.treinaweb.ediaristas.core.models.Servico;
+import br.com.treinaweb.ediaristas.core.repositories.ServicoRepository;
 
 @Controller
 @RequestMapping("/admin/servicos")
 public class ServicoController {
+
+
+    @Autowired
+    private ServicoRepository repository;
 
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar(){
@@ -19,6 +26,12 @@ public class ServicoController {
         return new ModelAndView("admin/servico/form")
         .addObject("servico", new Servico());
       
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Servico servico){
+        repository.save(servico);
+        return "redirect:/admin/servicos/cadastrar";
     }
 
     @ModelAttribute("icones")
