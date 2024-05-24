d4418c326e18ab67e364a64b9a17cd848b0476bf
Marcio Rodrigues <mrraxa01@gmail.com>
2024-05-24 01:16:51 -0300

    Implements WebServicoMapper

---
+++ src/main/java/br/com/treinaweb/ediaristas/web/mappers/WebServicoMapper.java
@@ -0,0 +1,31 @@
+package br.com.treinaweb.ediaristas.web.mappers;
+
+import org.springframework.stereotype.Component;
+
+import br.com.treinaweb.ediaristas.core.models.Servico;
+import br.com.treinaweb.ediaristas.web.dtos.ServicoForm;
+
+@Component
+public class WebServicoMapper {
+
+    public Servico toModel(ServicoForm servicoForm){
+        if (servicoForm == null) 
+            throw new IllegalArgumentException("Objeto inválido");
+    
+            return new Servico(null, servicoForm.getNome(), servicoForm.getValorMinimo(),servicoForm.getQtdHoras() ,
+            servicoForm.getPercentualComissao(), servicoForm.getHorasQuarto(), servicoForm.getValorQuarto(),
+            servicoForm.getHorasSala(), servicoForm.getValorSala(), servicoForm.getHorasBanheiro(),servicoForm.getValorBanheiro(),
+            servicoForm.getHorasCozinha(),servicoForm.getValorCozinha(),servicoForm.getHorasQuintal(), servicoForm.getValorQuintal(),
+            servicoForm.getHorasOutros(), servicoForm.getValorOutros(), servicoForm.getIcone(), servicoForm.getPosicao() 
+             );
+        }   
+    public ServicoForm toForm(Servico servico){
+        if (servico==null)
+            throw new IllegalArgumentException("Objeto inválido");
+        return new ServicoForm(servico.getNome(), servico.getValorMinimo(), servico.getQtdHoras(), servico.getPercentualComissao(), servico.getHorasQuarto(), servico.getValorQuarto(),
+        servico.getHorasSala(), servico.getValorSala(), servico.getHorasBanheiro(), servico.getValorBanheiro(), servico.getHorasCozinha(),
+        servico.getValorCozinha(), servico.getHorasQuintal(), servico.getValorQuintal(), servico.getHorasOutros(), servico.getValorOutros(),
+        servico.getIcone(), servico.getPosicao());
+    }
+
+}
