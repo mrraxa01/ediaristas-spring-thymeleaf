d7cd09345f1f4ea3a5bd6ebc854e11b09bdcb183
Marcio Rodrigues <mrraxa01@gmail.com>
2024-06-03 00:48:29 -0300

    add list users

---
+++ src/main/java/br/com/treinaweb/ediaristas/web/controllers/UsuarioController.java
@@ -0,0 +1,22 @@
+package br.com.treinaweb.ediaristas.web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.ediaristas.web.services.WebUsuarioService;
+
+@Controller
+@RequestMapping("admin/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private WebUsuarioService service;
+
+    @GetMapping
+    public ModelAndView findAll(){
+        return new ModelAndView("admin/usuarios/listar").addObject("usuarios", service.findAll());
+    }
+}
